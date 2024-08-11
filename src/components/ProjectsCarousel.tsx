82f250d9700834a377224542a3d2e949feac8b66
Ahmed-M-Sadek <ahmedms.abdelhamid@gmail.com>
2024-08-11 17:00:03 +0300

    fix too much hover scaling

--- src/components/ProjectsCarousel.tsx
+++ src/components/ProjectsCarousel.tsx
@@ -36,7 +36,7 @@ const ProjectsCarousel = () => {
             }) => (
               <CarouselItem
                 key={project.name}
-                className="flex-shrink-0 w-full sm:w-full md:basis-1/3 lg:basis-1/4 xl:basis-1/5 p-2 items-stretch duration-100 hover:scale-110"
+                className="flex-shrink-0 w-full sm:w-full md:basis-1/3 lg:basis-1/4 xl:basis-1/5 p-2 items-stretch"
               >
                 <ProjectCard project={project} />
               </CarouselItem>
